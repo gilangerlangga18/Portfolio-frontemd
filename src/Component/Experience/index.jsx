e69460af0384e7ec5a0e793fa77bf407e74b739c
gilangerlangga18 <gilang.erlangga1806@gmail.com>
2023-05-11 20:30:24 +0700

    new commit

--- src/Component/Experience/index.jsx
+++ src/Component/Experience/index.jsx
@@ -1,4 +1,3 @@
-import { Height } from "@mui/icons-material";
 import { Box, Typography } from "@mui/material";
 import React from "react";
 
--- src/Component/Project/index.jsx
+++ src/Component/Project/index.jsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import React, { useContext } from "react";
+import React from "react";
 import BoxCarousel from "../Carousel";
 import Dummy from "../../DummyData/DataProject.json";
 
